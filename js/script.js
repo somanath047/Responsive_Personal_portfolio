099ef4fa9cdb847e156fa55198a2d47269dcc842
Somanath Nayak <somanathnayak455@gmail.com>
2024-11-15 18:12:56 +0530

    added the JS file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*  ===================typing animation =======================*/
+var typed = new Typed(".typing", {
+        strings: ["", "Web Developer", "Backend Developer", "Graphic Designer", "Script Writer"],
+        typeSpeed: 100,
+        BackSpace: 60,
+        loop: true
+    })
+    /*  ===================typing animation =======================*/
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li")
+totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function() {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active")
+        }
+        this.classList.add("active")
+    })
+}
